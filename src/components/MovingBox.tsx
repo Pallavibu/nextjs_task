f1f4504407cfa5ffa28f30761ab8513e4a68df6e
Pallavi <pallavibu44@gmail.com>
2023-11-25 17:25:58 +0530

    moving box

--- src/components/MovingBox.tsx
+++ src/components/MovingBox.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import styles from './MovingBox.module.css';
 
 const MovingBox = () => {
-  return <div className={styles.box}></div>;
+  return <div className= {styles.box} >
+    
+    <div className='flex bg-pink-400 p-10'>
+       <img src="logo1.png" alt="logo1" />
+        <div className=''>
+            <h2>Pallavi</h2>
+            <a href="#">shop now</a>
+        </div>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    </div>
+   
+
 };
 
 export default MovingBox;
